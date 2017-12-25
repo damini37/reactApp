5aea15a65083242e5113cd82341bd10d77d36edc
Damini Jadaun <damini.jadaun@sophos.com>
2017-12-25 18:18:43 +0530

    Updating js

--- src/App2.js
+++ src/App2.js
@@ -1,101 +1,62 @@
 import React, {Component} from 'react'
 import SearchInput, {createFilter} from 'react-search-input';
-import $ from 'jquery'
 import iconImage from './icon.jpg'
 import _ from 'lodash'
-var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
+var itemNo;
 
-
-const countryData = ["INDIA", "USA", "ITALY", "JAPAN", "CHINA"];
-const selected = "";
-
-// import emails from './mails'
+const countryData = ["India", "Italy", "China", "USA"];
 
 const KEYS_TO_FILTERS = ['user.name', 'subject', 'dest.name']
 
-
+var CreateCountryLayout = createReactClass({displayName: "CreateCountryLayout",
+  render:function(){
+      console.log("Layout rendered")
+      var row = [];
+      for (var i = 0; i < 16; i++) {
+        row.push((<CreateCountryRow itemArray={this.props.itemArray} onClick={this.handleClick} ></CreateCountryRow>));
+      }
+      return (<div className="gridContainer" id="scrollDes">{row}</div>)
+  }
+});
 
 var CreateCountryRow = createReactClass({displayName: "CreateCountryRow",
-render:function(){
-  console.log("Row rendered")
-  var column = _.map(this.props.itemArray, (item, index) => {
-    return (<CreateCountryColumn key={index} onClick={this.handleClick} menuItem={item}></CreateCountryColumn>)
-  })
-  return (
-    <div className="row">{column}</div>
-  )
-  //  this.props.itemArray.forEach(function(menuItem, index){
-  //      column.push(React.createElement(createCountryColumn, {key: index, onClick: this.handleClick, menuItem: menuItem}));
-  //  }, this);
-  // return React.createElement("div", {className: "row"},  column);
-  }
+  render:function(){
+      console.log("Row rendered")
+      var column = [];
+      for (var i = 0; i < 4; i++) {
+        column.push((<CreateCountryColumn itemArray={this.props.itemArray} onClick={this.handleClick}></CreateCountryColumn>));
+      }
+      return (<div className="row">{column}</div>)
+    }
 })
 
 var CreateCountryColumn=createReactClass({displayName: "CreateCountryColumn",
 	handleClick:function(e){
-		this.props.onClick(e);
+
 	},
 	render: function() {
     console.log("Column rendered")
-    return (
-      <div className='col'>
-        <img src= {iconImage} alt={this.props.menuItem.label}/>
-      </div>
-    )
-			// var countryStr = (
-			// 		React.createElement("div", {className: "col"},
-			// 		React.createElement("div", "nameDiv",
-			// 		React.createElement("img", {src: require('./icon.jpg')}, this.props.menuItem.label)
-			// 	)));
-        // return countryStr;
+    console.log(itemNo)
+    itemNo++;
+    var val, icon;
+    if(this.props.itemArray[itemNo] != undefined) {
+      val = this.props.itemArray[itemNo];
+      icon = iconImage;
+    } else {
+      val="";
+      icon="";
+    }
+    console.log(icon);
+    var div = <div className="col">
+                  <div className="nameDiv">
+                    <img src= {icon}/>
+                    <span>{val}</span>
+                  </div>
+                </div>
+    return div;
     }
 });
-
-var CreateCountryLayout = createReactClass({displayName: "CreateCountryLayout",
-render:function(){
-  console.log("Layout rendered")
-  var row = _.map(this.props.itemArray, (item, index) => {
-    return (<CreateCountryRow key={index} onClick={this.handleClick} menuItem={item}></CreateCountryRow>)
-  })
-  return (
-    <div className="gridContainer">{row}</div>
-  )
-  // this.props.itemArray.forEach(function(menuItem, index){
-  //     row.push(React.createElement(createCountryRow, {key: index, onClick: this.handleClick, menuItem: menuItem}));
-  // }, this);
-  // return React.createElement("div", {className: "gridContainer"},  row);
-   }
-});
-
-class CountryName extends React.Component{
-  render(){
-    // return React.createElement("div", {className:"row"},
-    //         React.createElement("div", {className:"col"},
-    //           React.createElement("img", {src:"./icon.jpg"},
-    //             React.createElement("span", {className:"countryName"}, null)
-    //         )
-    //       )
-    //     );
-    // <Image src={require(ratingIconUrl)} style={styles.ratingImage} />
-    return <div className="row">
-      <div className="col"><div className="nameDiv"><img src={require('./icon.jpg')} /><span>name</span></div></div>
-      <div className="col"><div className="nameDiv"><img src={require('./icon.jpg')} /><span>name</span></div></div>
-      <div className="col"><div className="nameDiv"><img src={require('./icon.jpg')} /><span>name</span></div></div>
-      <div className="col"><div className="nameDiv"><img src={require('./icon.jpg')} /><span>name</span></div></div>
-
-    </div>;
-  }
-
-  createColumn = () => function() {
-    return React.createElement("div", {className:"col"},
-            React.createElement("img", {src:"./icon.jpg"},
-              React.createElement("span", {className:"countryName"}, null)
-            )
-          );
-    //return <div class="col"><img src="./icon.jpg" /><span>name</span></div>;
-  }
-}
 
 class App extends Component {
   constructor (props) {
@@ -105,7 +66,12 @@ class App extends Component {
     }
     this.searchUpdated = this.searchUpdated.bind(this)
   }
-
+  // addNewCountry:function(e){
+  //
+	// },
+  // delCountry:function(e){
+  //
+	// },
   render () {
     const filteredEmails = [{email:"s",user:{name:"ashis"},subject:"454"}];//emails.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
 
@@ -126,19 +92,15 @@ class App extends Component {
                   })}
                 </div>
 
-
-                <div className="gridContainer" id="scrollDes">
+                {itemNo=-1}
+                <div id="">
                   <CreateCountryLayout itemArray={countryData}></CreateCountryLayout>
-                  {/* <CountryName /><CountryName /><CountryName /><CountryName />
-                  <CountryName /><CountryName /><CountryName /><CountryName />
-                  <CountryName /><CountryName /><CountryName /><CountryName />
-                  <CountryName /><CountryName /><CountryName /><CountryName /> */}
                 </div>
 
                 <div className="btnDiv">
                   <div className="row">
-                    <button label="NEW" className="col newBtn">NEW</button>
-                    <button label="DELETE" className="col delBtn">DELETE</button>
+                    <button label="NEW" className="col newBtn" onClick={this.addNewCountry}>NEW</button>
+                    <button label="DELETE" className="col delBtn" onclick={this.delCountry}>DELETE</button>
                   </div>
                 </div>
             </div>
@@ -154,7 +116,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-//React.render(React.createElement(createCountryLayout, {}), document.getElementsByClassName('mainDiv'));
